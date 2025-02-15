aae3946403c80142965c066213ff1f83eb76ba2d
jordanjoelson <jjoelson007@gmail.com>
2025-02-14 23:41:51 -0600

    feat: get html and print, close after 2 min, authentication

--- index.js
+++ index.js
@@ -3,19 +3,34 @@ import puppet from 'puppeteer-core';
 async function run(){
     let browser;
     try{
+        const auth = 'brd-customer-hl_4709c740-zone-scraping_browser1:d5qrp8g0osv3'
+
+        const browserWSEndpoint = `wss://${auth}@brd.superproxy.io:9222`;
 
         browser = await puppet.connect({
-            browserWSEndpoint: 'wss:://'
+            browserWSEndpoint: browserWSEndpoint
         });
 
-       return;
+        const page = await browser.newPage();
+        page.setDefaultNavigationTimeout(2 * 60 * 1000); 
+
+        await page.goto('https://www.amazon.com');
+
+        const body = await page.$('body');
+
+        const html = await page.evaluate(() =>
+            document.documentElement.outerHTML
+        );
+
+        console.log(html);
+
+        return;
 
     } catch(e){
         console.error('scrape failed', e);
-    }
-    finally{
-        await browser?.close(); 
+    } finally{
+        await browser?.close();
     }
 }
 
-run()
+run();
